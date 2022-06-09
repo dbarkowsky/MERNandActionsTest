30eb094595f0c6856cecc4d4261881d32495de28
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2022-06-09 15:52:16 -0700

    true

--- server/tests/example.test.js
+++ server/tests/example.test.js
@@ -2,4 +2,8 @@ describe(`Let's try a test!`, () => {
     test(`Test #1`, () => {
         expect(false).toBeTruthy();
     });
+    
+    test(`Test #2`, () => {
+        expect(true).toBeTruthy();
+    });
 });
